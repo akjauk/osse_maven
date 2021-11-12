f55bcc82a30dc29ff29dcbec5e18f87569204299
akjauk <90042432+akjauk@users.noreply.github.com>
2021-11-12 10:37:11 +0100

    Add files via upload

---
+++ src/main/java/de/hfu/App.java
@@ -0,0 +1,23 @@
+package de.hfu;
+
+import java.util.Scanner;
+
+/**
+ * @author akjau
+ *	Class that includes the main for TextScanner
+ */
+public class App 
+{
+    /**
+     * @param args
+     * main method for scanning user input and returning it in upper case
+     */
+    public static void main( String[] args )
+    {
+        Scanner sc = new Scanner(System.in);  
+	    System.out.println("Enter a message: ");
+
+	    String message = sc.nextLine().toUpperCase();  
+	    System.out.println("Your message: \n" + message);
+    }
+}
